cf00a2ec07748a3213492e4bf994a36d2a4f6966
carlita2025itca-creator <carlita2025itca@gmail.com>
2026-08-28 13:01:47 -0500

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -128,7 +128,6 @@ onAuthStateChanged(auth, (user) => {
         actualizarDashboard();
         inicializarMenuPerfil();
     } else {
-        // window.location.href = "login.html"; // <-- LÍNEA COMENTADA (DESACTIVADA)
-        console.log("No hay sesión iniciada, pero no redirigimos para evitar errores.");
+      
     }
 });
